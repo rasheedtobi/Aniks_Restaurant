53711ede3a2a1333ef3713af5332eb26586a7dba
Rasheed Odunbaku <tobirasheed@gmail.com>
2023-05-27 22:20:05 +0200

    filter,search, order

--- AnikeAPI/views.py
+++ AnikeAPI/views.py
@@ -4,7 +4,7 @@ from .models import MenuItem
 from .serializers import MenuItemSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-
+from django.core.paginator import Paginator, EmptyPage
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -12,6 +12,32 @@ def menu_items(request):
     if request.method =='GET':
         # item = MenuItem.objects.all()
         item = MenuItem.objects.select_related('category').all() 
+
+        category_name = request.query_params.get('category')
+        to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
+        page= request.query_params.get('page', default=1)
+        perpage= request.query_params.get('perpage', default =2)
+
+
+        if category_name:
+            item = item.filter(category__menu_category = category_name)
+        if to_price:
+            item = item.filter(price__lte=to_price)
+        if search:
+            item = item.filter(title__istartswith = search)
+        if ordering:
+            # item =item.order_by(ordering)
+            ordering_fields = ordering.split(',')
+            item = item.order_by(*ordering_fields)
+
+        paginator = Paginator(item, per_page=perpage)
+        try:
+            item = paginator.page(number = page)
+        except EmptyPage:
+            item = []
+
         serialized_item = MenuItemSerializer(item, many=True)
         # return Response(items.values()) #without serializers
         return Response(serialized_item.data)
